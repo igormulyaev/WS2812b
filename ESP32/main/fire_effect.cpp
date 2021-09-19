7675ce8e67499d761a67b3c3457aa742cb0b453f
Igor Mulyaev <igor.mulyaev@gmail.com>
2021-09-19 23:37:23 +1000

    Add fire effect blank class

---
+++ ESP32/main/fire_effect.cpp
@@ -0,0 +1,18 @@
+﻿#include "fire_effect.h"
+#include "led_params.h"
+
+// -----------------------------------------------------
+void CFireEffect :: OnTimer() {
+  FastLED.show();
+};
+
+// -----------------------------------------------------
+void CFireEffect :: OnStart() {
+  for (int pos = 0; pos < NUM_LEDS; ++pos) {
+    leds[pos] = CRGB::Black;
+  }
+}
+
+// -----------------------------------------------------
+CFireEffect :: ~CFireEffect() {
+}
---
+++ ESP32/main/fire_effect.h
@@ -0,0 +1,16 @@
+﻿#ifndef M_FIRE_EFFECT_H
+#define M_FIRE_EFFECT_H
+
+#include <cstdint>
+#include "led_effect.h"
+#include "FastLED.h"
+
+class CFireEffect: public CLedEffect {
+  private:
+
+  public:
+    virtual void OnStart();
+    virtual void OnTimer();
+    virtual ~CFireEffect();
+};
+#endif
