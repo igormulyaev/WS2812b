a1919b1386152b07efdd3cd149171ab29151549d
Igor Mulyaev <igor.mulyaev@gmail.com>
2022-12-15 18:01:17 +1000

    Add Director class

---
+++ ESP32/main/director.h
@@ -0,0 +1,15 @@
+﻿#ifndef DIRECTOR_H
+#define DIRECTOR_H
+
+class Director
+{
+  public:
+    Director ();
+    ~Director ();
+
+  private:
+    Director (const Director &) = delete;
+    Director & operator=(const Director &) = delete;
+};
+
+#endif
